eb902b471a85616b354323cd3c5c7ce6849bc298
md_front <den_archangel@mail.ru>
2020-10-28 14:14:42 +0500

    Updates

---
+++ precache-manifest.4a53a88634dda2156c567b197dd21af2.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02f4a27862aa6bfc09211b81c254b90a",
+    "url": "/_test_react/index.html"
+  },
+  {
+    "revision": "10bcf2230d92ade350c3",
+    "url": "/_test_react/static/css/main.cc2b94e5.chunk.css"
+  },
+  {
+    "revision": "cb21d817234236958626",
+    "url": "/_test_react/static/js/2.8310ca70.chunk.js"
+  },
+  {
+    "revision": "65075b20798fa24012a1ad453c78c90d",
+    "url": "/_test_react/static/js/2.8310ca70.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "10bcf2230d92ade350c3",
+    "url": "/_test_react/static/js/main.34530ab1.chunk.js"
+  },
+  {
+    "revision": "e0c2b26eba211c18a2f7",
+    "url": "/_test_react/static/js/runtime-main.414419b2.js"
+  },
+  {
+    "revision": "0ce28e41764f78aa15105a519105dcd2",
+    "url": "/_test_react/static/media/del.0ce28e41.svg"
+  },
+  {
+    "revision": "80b7ccfb2ed9af0292b72510c13428bd",
+    "url": "/_test_react/static/media/star.80b7ccfb.svg"
+  }
+]);
